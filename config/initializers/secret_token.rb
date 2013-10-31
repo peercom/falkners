35f47284f7e01b1658ed7aeddb578d720465a608
Holger von Ameln <holger.von.ameln@peercom.org>
2013-10-31 11:17:56 +0100

    initial commit changed original layout to scale vertically added inquieries added page_images added blog for news

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Falkners::Application.config.secret_token = 'd3ea2f9ea040cf6a9ab8b98819260229e2b4ed1a6827df14003bc9e713e4a8aa2732e4d6b5668b414a93fd5801e2987fc97c0e424e53786c7959f9c5604262b7'
