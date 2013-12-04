d2e7b8f7a166bccc0f43d38c5f9d98af0b5c13c7
Holger von Ameln <holger.von.ameln@peercom.org>
2013-12-04 19:20:31 +0100

    add capistrano config

---
+++ config/deploy.rb
@@ -0,0 +1,46 @@
+set :application, 'falners'
+
+set :rvm_ruby_string, '1.9.3@refinery'        # Or whatever env you want it to run in.
+set :rvm_type, :user
+
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+set :scm, :git
+set :repository, "./.git"
+set :local_repository, './.git'
+set :deploy_via, :copy
+set :git_shallow_clone, 1
+set :scm_verbose, true
+
+# set :format, :pretty
+# set :log_level, :debug
+# set :pty, true
+
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+# set :keep_releases, 5
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+  after :finishing, 'deploy:cleanup'
+
+end
